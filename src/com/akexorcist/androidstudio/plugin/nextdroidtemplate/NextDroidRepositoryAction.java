dbaec507f586e426c7234097bea0b28dbd63de29
Akexorcist <akexorcist@gmail.com>
2017-10-25 08:04:10 +0700

    Create action class

---
+++ src/com/akexorcist/androidstudio/plugin/nextdroidtemplate/NextDroidRepositoryAction.java
@@ -0,0 +1,12 @@
+package com.akexorcist.androidstudio.plugin.nextdroidtemplate;
+
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class NextDroidRepositoryAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+        System.out.print(ActionManager.getInstance().getId(this) + " was clicked");
+    }
+}
